4d1d93933c943305a98c0f3d6fd824d0a07f3d49
liuderchi <liuderchi@gmail.com>
2017-09-12 04:47:20 +0800

    lec 5: multiplate DOM from stream

--- reactive-programming/github-users-gen/index.js
+++ reactive-programming/github-users-gen/index.js
@@ -1,25 +1,52 @@
-// Introduction to Reactive Programming - Lec 4
+const refreshButton = document.querySelector('.refresh')
 
-// https://egghead.io/lessons/rxjs-use-rxjs-async-requests-and-responses
+const requestStream$ = Rx.Observable.of('https://api.github.com/users')
 
-const { Observable } = require('rxjs/Rx')
-const axios = require('axios')
+const responseStream$ = requestStream$
+  // .flatMap(requestUrl =>  // NOTE flatMap is now an alias for mergeMap
+  // but will work just the same.
+  .mergeMap(requestUrl =>
+    Rx.Observable.fromPromise(jQuery.getJSON(requestUrl))
+  )
+// NOTE for the only reqUrl in requestStream, generate corresponding
+// meta stream Observable then use flatMap to flatten it
+//   - we must generate another stream since we need do async request
 
-const request$ = Observable.of('https://api.github.com/users')
-// NOTE simple stream with one event of string value
+const createSuggestionStream = resp$ => (
+  resp$.map(listUser =>
+    listUser[Math.floor(Math.random()*listUser.length)]
+))
+// map the resolved/rejected value of promise observable
+// into another value: random chosen user object, as a 'suggestion' stream
 
-// OPT 1. use map()
-const response$ = request$
-  // .map(reqUrl => Observable.fromPromise(axios.get(reqUrl))) // NOTE just returns a promise as event value
-  // .flatMap(reqUrl => Observable.fromPromise(axios.get(reqUrl)))  // NOTE works but deprecate
-  .concatMap(reqUrl => Observable.fromPromise(axios.get(reqUrl)))  // NOTE Rx5 api, flatten a stream
-// NOTE concatMap is like .then of promise
 
-response$.subscribe(resp => console.log(resp.data.map(usr => usr.login)))
+const suggestion1Stream$ = createSuggestionStream(responseStream$)
+const suggestion2Stream$ = createSuggestionStream(responseStream$)
+const suggestion3Stream$ = createSuggestionStream(responseStream$)
 
-// OPT 2. BAD: create a new stream in sub Cb (create meta stream (forked))
-// NOTE with concatMap(0) you don't have to subscribe inside a subscribe Cb (nested subscribe)
-// request$.subscribe(reqUrl => {
-//   response$ = Observable.fromPromise(axios.get(reqUrl))
-//   response$.subscribe(resp => console.log(resp.data.map(usr => usr.login)))
-// })
+function renderSuggestion(userData, selector) {
+  const element = document.querySelector(selector)
+  const usernameEl = element.querySelector('.username')
+  usernameEl.href = userData.html_url
+  usernameEl.textContent = userData.login
+  const imgEl = element.querySelector('img')
+  imgEl.src = userData.avatar_url
+}
+
+// subscribe each suggestion stream with Cb to manipulate DOM
+suggestion1Stream$.subscribe(user => {
+  renderSuggestion(user, '.suggestion1')
+})
+suggestion2Stream$.subscribe(user => {
+  renderSuggestion(user, '.suggestion2')
+})
+suggestion3Stream$.subscribe(user => {
+  renderSuggestion(user, '.suggestion3')
+})
+
+
+// NOTE raw flow of Observable api
+// url ---of()--> observable-of-string
+// ---mergeMap(()=>Observable.fromPromise())--> observable-of-promise
+// ---map()--> observable-of-userData
+// ---subscribe(domOp)--> subscription stream starts
